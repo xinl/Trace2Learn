081bc0f9e7f23efbdae2c14870c2c0fcd89316bd
Takuya Yamashita <takuyay@seas.upenn.edu>
2012-10-13 02:49:57 -0700

    modify tagActivity to reflect the tag edit change immediately

--- src/edu/upenn/cis350/Trace2Learn/TagActivity.java
+++ src/edu/upenn/cis350/Trace2Learn/TagActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -201,6 +202,11 @@ public class TagActivity extends Activity {
 	public void onDestroy() {
 		mDbHelper.close();
 		super.onDestroy();
+	}
+	
+	@Override
+	public void onBackPressed() {
+	    startActivity(new Intent(this, BrowseCharactersActivity.class));
 	}
 
 }
