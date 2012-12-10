6437f9b2e539994bcbf915bfca5948dcfeaa26bc
Xin Li <mr.xin.li@gmail.com>
2012-12-09 23:50:41 -0500

    Add import data integrity check; add entity counts in confirmation dialog

--- src/edu/upenn/cis350/Trace2Learn/ImportActivity.java
+++ src/edu/upenn/cis350/Trace2Learn/ImportActivity.java
@@ -2,7 +2,9 @@ package edu.upenn.cis350.Trace2Learn;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -42,8 +44,8 @@ public class ImportActivity extends Activity {
 	private String subFolder, extStorageDirectory;
 	private File folderPath;
 	private Context thisContext;
-	
-	//Controls
+
+	// Controls
 	private ListView lv;
 
 	@Override
@@ -61,54 +63,50 @@ public class ImportActivity extends Activity {
 		subFolder = "/Trace2Learn";
 		extStorageDirectory = Environment.getExternalStorageDirectory().toString();
 		folderPath = new File(extStorageDirectory + subFolder);
-		if(!folderPath.exists()) {
+		if (!folderPath.exists()) {
 			folderPath.mkdirs();
 		}
-		
+
 		fileList = listFileRecursively(folderPath);
 		for (File file : fileList) {
 			fileNameList.add(file.getName());
 		}
 
-		arrAdapter = new ArrayAdapter<String>(thisContext, 
-				android.R.layout.simple_list_item_1, fileNameList);
+		arrAdapter = new ArrayAdapter<String>(thisContext, android.R.layout.simple_list_item_1, fileNameList);
 		arrAdapter.notifyDataSetChanged();
 
 		lv.setAdapter(arrAdapter);
 		lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-		lv.setOnItemClickListener(new OnItemClickListener(){
+		lv.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> adapter, View view, int position,
-					long this_id) {
-				AlertDialog dialog = (AlertDialog) onCreateDialog(position);
-				dialog.show();
-			}});
-		setTitle(getTitle() + " È Import");
-	}
-	
-	protected Dialog onCreateDialog(int position) {
-		final int index = position;
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(R.string.importXml_confirm);
-		builder.setCancelable(false);
-		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {		
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				File importFile = fileList.get(index);
+			public void onItemClick(AdapterView<?> adapter, View view, int position, long this_id) {
+				File importFile = fileList.get(position);
 				try {
 					importXML(importFile);
-					Toast.makeText(thisContext, "Import successful.", 
-							Toast.LENGTH_LONG).show();
 				} catch (Exception e) {
-				    Log.e("IMPORTING", e.getMessage());
-				    Toast.makeText(thisContext, "Import failed.", 
-							Toast.LENGTH_LONG).show();
+					Log.e("IMPORTING", e.getMessage());
+					Toast.makeText(thisContext, "Import failed.", Toast.LENGTH_LONG).show();
 				}
 			}
 		});
-		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {		
+		setTitle(getTitle() + " È Import");
+	}
+
+	protected Dialog createDialog(final List<Character> characters, final List<Word> words, final List<Collection> collections) {
+		String message = String.format(getResources().getString(R.string.importXml_confirm, characters.size(), words.size(), collections.size()));
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message);
+		builder.setCancelable(false);
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				saveData(characters, words, collections);
+				Toast.makeText(thisContext, "Import successful.", Toast.LENGTH_LONG).show();
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.cancel();
@@ -116,156 +114,196 @@ public class ImportActivity extends Activity {
 		});
 		return builder.create();
 	}
-	
+
 	public void importXML(File xmlFile) throws Exception {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(xmlFile);
 		doc.getDocumentElement().normalize();
-		
+
 		NodeList characterNodes = doc.getElementsByTagName("character");
 		NodeList wordNodes = doc.getElementsByTagName("word");
 		NodeList collectionNodes = doc.getElementsByTagName("collection");
-		
+
 		List<Character> characters = new ArrayList<Character>();
 		for (int i = 0; i < characterNodes.getLength(); i++) {
 			Element element = (Element) characterNodes.item(i);
 			characters.add(createCharacter(element));
 		}
-		
+
 		List<Word> words = new ArrayList<Word>();
 		for (int i = 0; i < wordNodes.getLength(); i++) {
 			Element element = (Element) wordNodes.item(i);
 			words.add(createWord(element));
 		}
-		
+
 		List<Collection> collections = new ArrayList<Collection>();
 		for (int i = 0; i < collectionNodes.getLength(); i++) {
 			Element element = (Element) collectionNodes.item(i);
 			collections.add(createCollection(element));
 		}
-		
-		//TODO: verify data integrity here
-		
-		for (Character c: characters) {
+
+		// verify data integrity here
+		if (checkDataIntegrity(characters, words, collections) == false) {
+			Toast.makeText(thisContext, "Invalid data file format.", Toast.LENGTH_LONG).show();
+			return;
+		}
+				
+		AlertDialog dialog = (AlertDialog) createDialog(characters, words, collections);
+		dialog.show();
+	}
+	
+	private void saveData(List<Character> characters, List<Word> words, List<Collection> collections) {
+		for (Character c : characters) {
 			dba.updateCharacter(c);
 		}
-		
-		for (Word w: words) {
+
+		for (Word w : words) {
 			dba.updateWord(w);
 		}
-		
-		for (Collection cl: collections) {
+
+		for (Collection cl : collections) {
 			dba.updateCollection(cl);
 		}
 	}
-	
+
+	private boolean checkDataIntegrity(List<Character> characters, List<Word> words, List<Collection> collections) {
+		Set<Long> charIDs = new HashSet<Long>();
+		for (Character ch : characters) {
+			charIDs.add(ch.getId());
+		}
+
+		Set<Long> wordIDs = new HashSet<Long>();
+		for (Word w : words) {
+			wordIDs.add(w.getId());
+			if (charIDs.containsAll(w.getCharacterIds()) == false) {
+				return false;
+			}
+		}
+		
+		for (Collection c : collections) {
+			if (wordIDs.containsAll(c.getWordIds()) == false) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	/**
 	 * Create a single character item
-	 * @param character an element in an xml document representing a character
+	 * 
+	 * @param character
+	 *            an element in an xml document representing a character
 	 * @return the character based on the xml
 	 */
 	static Character createCharacter(Element character) {
 		Character c = new Character();
-		//get attributes
-	    long id = Long.valueOf(character.getAttribute("id"));
-	    c.setId(id);
-	    long order = Long.valueOf(character.getAttribute("order"));
-	    c.setOrder(order);
-	    
-	    String strokesString = getValue(character.getElementsByTagName("strokes").item(0));
-	    byte[] strokesData = Base64.decode(strokesString, Base64.DEFAULT);
-	    List<Stroke> strokes = Stroke.decodeStrokesData(strokesData);
-	    c.setStrokes(strokes);
-	    
-	    NodeList tagNodes = character.getElementsByTagName("tag");
-	    for (int i = 0; i < tagNodes.getLength(); i++) {
-	    	String tag = getValue(tagNodes.item(i));
-	    	c.addTag(tag);
-	    }
-	    
-	    NodeList attributeNodes = character.getElementsByTagName("attribute");
-	    for (int i = 0; i < attributeNodes.getLength(); i++) {
-	    	Element attribute = (Element) attributeNodes.item(i);
-	    	String type = attribute.getAttribute("type");
-	    	String attValue = getValue(attribute);
-	    	c.addAttribute(type, attValue);
-	    }
-	    
+		// get attributes
+		long id = Long.valueOf(character.getAttribute("id"));
+		c.setId(id);
+		long order = Long.valueOf(character.getAttribute("order"));
+		c.setOrder(order);
+
+		String strokesString = getValue(character.getElementsByTagName("strokes").item(0));
+		byte[] strokesData = Base64.decode(strokesString, Base64.DEFAULT);
+		List<Stroke> strokes = Stroke.decodeStrokesData(strokesData);
+		c.setStrokes(strokes);
+
+		NodeList tagNodes = character.getElementsByTagName("tag");
+		for (int i = 0; i < tagNodes.getLength(); i++) {
+			String tag = getValue(tagNodes.item(i));
+			c.addTag(tag);
+		}
+
+		NodeList attributeNodes = character.getElementsByTagName("attribute");
+		for (int i = 0; i < attributeNodes.getLength(); i++) {
+			Element attribute = (Element) attributeNodes.item(i);
+			String type = attribute.getAttribute("type");
+			String attValue = getValue(attribute);
+			c.addAttribute(type, attValue);
+		}
+
 		return c;
 	}
-	
+
 	/**
 	 * Create a single word item
-	 * @param ele an element in an xml document representing a word
+	 * 
+	 * @param ele
+	 *            an element in an xml document representing a word
 	 * @return the word based on the xml
 	 */
 	static Word createWord(Element ele) {
 		Word word = new Word();
-		
-		//get attributes
-	    long id = Long.valueOf(ele.getAttribute("id"));
-	    word.setId(id);
-	    long order = Long.valueOf(ele.getAttribute("order"));
-	    word.setOrder(order);
-	    
-	    NodeList tagNodes = ele.getElementsByTagName("tag");
-	    for (int i = 0; i < tagNodes.getLength(); i++) {
-	    	String tag = getValue(tagNodes.item(i));
-	    	word.addTag(tag);
-	    }
-	    
-	    NodeList attributeNodes = ele.getElementsByTagName("attribute");
-	    for (int i = 0; i < attributeNodes.getLength(); i++) {
-	    	Element attribute = (Element) attributeNodes.item(i);
-	    	String type = attribute.getAttribute("type");
-	    	String attValue = getValue(attribute);
-	    	word.addAttribute(type, attValue);
-	    }
-	    
-	    NodeList charidNodes = ele.getElementsByTagName("charid");
-	    for (int i = 0; i < charidNodes.getLength(); i++) {
-	    	long charid = Long.valueOf(getValue(charidNodes.item(i)));
-	    	Character ch = new Character();
-	    	ch.setId(charid);
-	    	word.addCharacter(ch);
-	    }
-	    
+
+		// get attributes
+		long id = Long.valueOf(ele.getAttribute("id"));
+		word.setId(id);
+		long order = Long.valueOf(ele.getAttribute("order"));
+		word.setOrder(order);
+
+		NodeList tagNodes = ele.getElementsByTagName("tag");
+		for (int i = 0; i < tagNodes.getLength(); i++) {
+			String tag = getValue(tagNodes.item(i));
+			word.addTag(tag);
+		}
+
+		NodeList attributeNodes = ele.getElementsByTagName("attribute");
+		for (int i = 0; i < attributeNodes.getLength(); i++) {
+			Element attribute = (Element) attributeNodes.item(i);
+			String type = attribute.getAttribute("type");
+			String attValue = getValue(attribute);
+			word.addAttribute(type, attValue);
+		}
+
+		NodeList charidNodes = ele.getElementsByTagName("charid");
+		for (int i = 0; i < charidNodes.getLength(); i++) {
+			long charid = Long.valueOf(getValue(charidNodes.item(i)));
+			Character ch = new Character();
+			ch.setId(charid);
+			word.addCharacter(ch);
+		}
+
 		return word;
 	}
-	
+
 	/**
 	 * Create a single collection item
-	 * @param ele an element in an xml document representing a collection
+	 * 
+	 * @param ele
+	 *            an element in an xml document representing a collection
 	 * @return the collection based on the xml
 	 */
 	static Collection createCollection(Element ele) {
 		Collection collection = new Collection();
-		
-		//get attributes
-	    long id = Long.valueOf(ele.getAttribute("id"));
-	    collection.setId(id);
-	    
-	    NodeList nameNodes = ele.getElementsByTagName("name");
-	    collection.setName(getValue(nameNodes.item(0)));
-	    
-	    NodeList descriptionNodes = ele.getElementsByTagName("description");
-	    collection.setDescription(getValue(descriptionNodes.item(0)));
-	    
-	    NodeList wordidNodes = ele.getElementsByTagName("wordid");
-	    for (int i = 0; i < wordidNodes.getLength(); i++) {
-	    	long wordid = Long.valueOf(getValue(wordidNodes.item(i)));
-	    	Word w = new Word();
-	    	w.setId(wordid);
-	    	collection.addWord(w);
-	    }
-	    
+
+		// get attributes
+		long id = Long.valueOf(ele.getAttribute("id"));
+		collection.setId(id);
+
+		NodeList nameNodes = ele.getElementsByTagName("name");
+		collection.setName(getValue(nameNodes.item(0)));
+
+		NodeList descriptionNodes = ele.getElementsByTagName("description");
+		collection.setDescription(getValue(descriptionNodes.item(0)));
+
+		NodeList wordidNodes = ele.getElementsByTagName("wordid");
+		for (int i = 0; i < wordidNodes.getLength(); i++) {
+			long wordid = Long.valueOf(getValue(wordidNodes.item(i)));
+			Word w = new Word();
+			w.setId(wordid);
+			collection.addWord(w);
+		}
+
 		return collection;
 	}
-	
+
 	/**
 	 * Retrieve the value of a node from a document
-	 * @param node the node whose value will be retrieved
+	 * 
+	 * @param node
+	 *            the node whose value will be retrieved
 	 * @return the value of the node as a string
 	 */
 	static private String getValue(Node node) {
@@ -273,7 +311,7 @@ public class ImportActivity extends Activity {
 		if (children.getLength() == 0) {
 			return "";
 		}
-	    return children.item(0).getNodeValue();
+		return children.item(0).getNodeValue();
 	}
 
 	@Override
@@ -281,7 +319,7 @@ public class ImportActivity extends Activity {
 		dba.close();
 		super.onDestroy();
 	}
-	
+
 	private List<File> listFileRecursively(File root) {
 		List<File> fileList = new ArrayList<File>();
 		File[] files = root.listFiles();
